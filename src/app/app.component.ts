24acc0b0de7ba1f7e6ab6f3cb2e0fc1631dbf745
Kris <pranger.kris@gmail.com>
2022-08-05 17:11:48 -0400

    AngularToDoLab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularToDo';
+
+  todoList: Todo[] = [
+    {
+      task: "fold clothes",
+      complete: false
+    },
+    {
+      task: "put clothes in dresser",
+      complete: false
+    },
+    {
+      task: "relax",
+      complete: true
+    },
+    {
+      task: "try to pet cat",
+      complete: false
+    },
+    {
+      task: "pet dog",
+      complete: false
+    },
+    {
+      task: "be awesome",
+      complete: false
+    },
+  ]
+
+  addTask(form:NgForm):void{
+    let newTask:Todo = {
+      task: form.form.value.task,
+      complete: false
+    }
+    this.todoList.push(newTask);
+  };
+
+  completeTask():void{
+
+  }
+
+  removeTask(index:number):void{
+    if(index > -1){
+      this.todoList.splice(index,1)
+    }
+  }
 }
